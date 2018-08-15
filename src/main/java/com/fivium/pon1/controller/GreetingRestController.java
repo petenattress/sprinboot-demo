45792e64b869a682e06004bd47957113830755aa
Pete Nattress <pete.nattress@fivium.co.uk>
2018-08-15 15:05:36 +0100

    Add thymeleaf HTML response

--- src/main/java/com/fivium/pon1/controller/GreetingRestController.java
+++ src/main/java/com/fivium/pon1/controller/GreetingRestController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+@RequestMapping("/rest")
+public class GreetingRestController {
 
   @RequestMapping(value = "/greeting", method = GET)
   public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
