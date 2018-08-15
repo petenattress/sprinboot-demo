fae01578367918213d3c745f1abe54394317b431
Pete Nattress <pete.nattress@fivium.co.uk>
2018-08-15 11:54:55 +0100

    Initial commit (demo REST app)

---
+++ src/main/java/com/fivium/pon1/controller/GreetingController.java
@@ -0,0 +1,18 @@
+package com.fivium.pon1.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import com.fivium.pon1.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+  @RequestMapping(value = "/greeting", method = GET)
+  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    return new Greeting(1, String.format("Hello, %s", name));
+  }
+
+}
