8d8309f39ac92c0c6aee365622eb19b759879479
Pete Nattress <pete.nattress@fivium.co.uk>
2018-08-17 11:19:43 +0100

    Spring Security for CSRF

--- src/main/java/com/fivium/pon1/config/WebConfig.java
+++ src/main/java/com/fivium/pon1/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.fivium.pon1;
+package com.fivium.pon1.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
