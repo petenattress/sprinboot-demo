2635298bc9a3a672c828642bf6d5a5fdc1ed2c2c
Pete Nattress <pete.nattress@fivium.co.uk>
2018-08-22 12:09:47 +0100

    Camunda/JPA transaction with inconsistency if JPA fails

--- src/main/java/com/fivium/pon1/controller/CamundaController.java
+++ src/main/java/com/fivium/pon1/controller/CamundaController.java
@@ -1,28 +1,29 @@
 package com.fivium.pon1.controller;
 
-import org.camunda.bpm.engine.RepositoryService;
-import org.camunda.bpm.engine.RuntimeService;
+import com.fivium.pon1.service.CamundaAndJpaTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/camunda")
 public class CamundaController {
 
-  private final RuntimeService runtimeService;
-  private final RepositoryService repositoryService;
+  private final CamundaAndJpaTransactionService camundaAndJpaTransactionService;
 
   @Autowired
-  public CamundaController(RuntimeService runtimeService, RepositoryService repositoryService) {
-    this.runtimeService = runtimeService;
-    this.repositoryService = repositoryService;
+  public CamundaController(CamundaAndJpaTransactionService camundaAndJpaTransactionService) {
+    this.camundaAndJpaTransactionService = camundaAndJpaTransactionService;
   }
 
   @GetMapping("/start")
-  public void startProcess() {
-    runtimeService.startProcessInstanceByKey("mySimpleProcess");
+  @ResponseBody
+  public String startProcess(@RequestParam("fail") boolean fail) {
+    camundaAndJpaTransactionService.startAndAddPerson(fail);
+    return "OK";
   }
 
 }
--- src/main/java/com/fivium/pon1/service/CamundaAndJpaTransactionService.java
+++ src/main/java/com/fivium/pon1/service/CamundaAndJpaTransactionService.java
@@ -1,4 +1,38 @@
 package com.fivium.pon1.service;
 
+import com.fivium.pon1.model.persistence.Person;
+import com.fivium.pon1.repository.PersonRepository;
+import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CamundaAndJpaTransactionService {
+
+  private final RuntimeService runtimeService;
+  private final RepositoryService repositoryService;
+  private final PersonRepository personRepository;
+
+  @Autowired
+  public CamundaAndJpaTransactionService(RuntimeService runtimeService, RepositoryService repositoryService,
+                                         PersonRepository personRepository) {
+    this.runtimeService = runtimeService;
+    this.repositoryService = repositoryService;
+    this.personRepository = personRepository;
+  }
+
+  public void startAndAddPerson(boolean fail) {
+
+    Person person = new Person();
+    person.setName("dummy person");
+    personRepository.save(person);
+
+    if (fail) {
+      throw new RuntimeException("oh noes!");
+    }
+
+    runtimeService.startProcessInstanceByKey("mySimpleProcess");
+
+  }
 }
